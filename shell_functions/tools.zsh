4062b8c0cb441536b51c19c9605c92f77d87900a
Simon Collins <874670+simoncollins@users.noreply.github.com>
2025-11-16 17:55:24 +1000

    Sort tool list
    Update 'tool' shell function to sort the list of tools shown
    alphabetically

--- shell_functions/tools.zsh
+++ shell_functions/tools.zsh
@@ -1,6 +1,6 @@
 tools() {
   echo
-  grep '^[[:blank:]]*#' ~/.config/mise/config.toml | sed 's/^[[:blank:]]*# *//' | sed $'s/^\\([^:]*\\):/\033[1;34m\\1\033[0m:/'
+  grep '^[[:blank:]]*#' ~/.config/mise/config.toml | sed 's/^[[:blank:]]*# *//' | sed $'s/^\\([^:]*\\):/\033[1;34m\\1\033[0m:/' | sort
   echo
 }
 
